cd2eefefd70f6f3581e39340a68c7c9a36f8dca9
Vikingdev13 <vikingdeveloper13@gmail.com>
2021-03-25 15:09:34 -0400

    Initial Commit

---
+++ TL;DR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TL;DR
+//
+//  Created by John Piccione on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
